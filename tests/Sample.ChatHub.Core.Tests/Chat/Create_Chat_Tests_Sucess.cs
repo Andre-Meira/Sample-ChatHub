db8a2a746d2b248a51cf84a211789386923c265f
Andre Meira <andre._.meira@hotmail.com>
2024-02-08 16:34:36 -0400

    Criação de testes unitarios do chat, falta criar alguns ainda.

---
+++ tests/Sample.ChatHub.Core.Tests/Chat/Create_Chat_Tests_Sucess.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using Sample.ChatHub.Core.Chat;
+using Sample.ChatHub.Core.Chat.Events;
+
+namespace Sample.ChatHub.Core.Tests.Chat;
+
+public class Create_Chat_Tests_Sucess
+{
+    #region Mocks
+    private Guid id = Guid.NewGuid();
+
+    private readonly IChatProcessStream _chatProcess;    
+    private readonly IChatEventsRepositore _chatEventsRepositore;    
+    private readonly IList<IChatEventStream>  chatEventStreams = new List<IChatEventStream>();
+
+    public Create_Chat_Tests_Sucess()
+    {        
+        var chatRepositoreRepositore = new Mock<IChatEventsRepositore>();                
+
+        _chatEventsRepositore = chatRepositoreRepositore.Object;
+        _chatProcess = new ChatProcessStream(_chatEventsRepositore);
+    }
+    #endregion
+
+
+    [Fact(DisplayName = "Criação do chat")]    
+    public async Task Criar_Chat_Com_SucessoAsync()
+    {
+        ChatCreated chatCreated = new ChatCreated("Chat Teste", id);
+        await _chatProcess.Include(chatCreated);        
+    }
+
+}
