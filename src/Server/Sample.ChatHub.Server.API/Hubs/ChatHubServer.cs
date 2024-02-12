6a1879c904aa380b28b7bf8ab5349a5e591c2613
Andre Meira <andre._.meira@hotmail.com>
2024-02-12 08:59:50 -0400

    Criando um abstracao de um hub para pega informações do usuario, e inclusao de processameto das messages e eventos do chathub

--- src/Server/Sample.ChatHub.Server.API/Hubs/ChatHubServer.cs
+++ src/Server/Sample.ChatHub.Server.API/Hubs/ChatHubServer.cs
@@ -1,44 +1,53 @@
-﻿using System;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Sample.ChatHub.Bus;
 using Sample.ChatHub.Domain.Contracts;
+using Sample.ChatHub.Domain.Contracts.Chat;
+using Sample.ChatHub.Domain.Contracts.Messages;
 
 namespace Sample.ChatHub.Server.API;
 
 [Authorize]
-public class ChatHubServer : Hub
+public class ChatHubServer : BaseHub<IChatHub>
 {
     private readonly ILogger<ChatHubServer> _logger;
+    private readonly IPublishContext _context;
 
-    public ChatHubServer(ILogger<ChatHubServer> logger)
+    public ChatHubServer(ILogger<ChatHubServer> logger, IPublishContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public override async Task OnConnectedAsync()
     {
-        string? user = this.Context.User?.FindFirst(ClaimTypes.Name)?.Value;
+        _logger.LogInformation("{0} Connected in server", UserName);
 
-        _logger.LogInformation("{0} Connected in server", user);
-
-        var context = new ReceiveMessageContext(Guid.NewGuid(), Guid.NewGuid(), "Sistema", $"Bem vindo ao chat {user}");
-        await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", context);
+        var context = new ReceiveMessageContext(Guid.Empty, Guid.Empty, "Sistema", $"Bem vindo ao chat {UserName!}");        
+        await Clients.Client(Context.ConnectionId).ReceiveMessage(context);
     }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        _logger.LogInformation("{0} User disconnected", UserName);
+        return base.OnDisconnectedAsync(exception);
+    }
+
 
     public async Task JoineChat(Guid guid)
     {
+        await _context.PublishMessage(new UserJoinChat(guid, UserId)).ConfigureAwait(false);
         await Groups.AddToGroupAsync(Context.ConnectionId, guid.ToString());
-    }
+    }    
 
     public async Task SendMessage(Guid idChat, string message)
     {
-        string user = this.Context.User!.FindFirst(ClaimTypes.Name)!.Value;
-        Guid guid = Guid.Parse(this.Context.User!.FindFirst(ClaimTypes.Sid)!.Value);
-        
-        var context = new ReceiveMessageContext(idChat, guid, user, message);
-        IClientProxy client = Clients.GroupExcept(idChat.ToString(), Context.ConnectionId);
+        var context = new ReceiveMessageContext(idChat,UserId, UserName!, message);
 
-        await client.SendAsync("ReceiveMessage",context);
+        var contractMessage = new SendMessage(idChat, UserId, message);
+        await _context.PublishMessage(contractMessage).ConfigureAwait(false);
+
+        var client = Clients.GroupExcept(idChat.ToString(), Context.ConnectionId);
+        await client.ReceiveMessage(context);
     }
 }
---
+++ src/Worker/Sample.ChatHub.Worker/Consumers/SendMessageHandlerConsumer.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using Sample.ChatHub.Bus;
+using Sample.ChatHub.Core.Chat;
+using Sample.ChatHub.Core.Chat.Events;
+using Sample.ChatHub.Domain.Contracts.Messages;
+
+namespace Sample.ChatHub.Worker.Consumers;
+
+internal sealed class SendMessageHandlerConsumer : ConsumerHandlerBase<SendMessage>
+{
+    private readonly IChatProcessStream _chatProcess;
+
+    public SendMessageHandlerConsumer(
+        IConnectionFactory connectionFactory, 
+        IChatProcessStream chatProcess) 
+    : base(connectionFactory)
+    {
+        this.ExchageType = ExchangeType.Direct;
+        this.PrefetchCount = 20;
+        this.ExchangeName = "send-message-consumer";
+        _chatProcess = chatProcess;
+    }
+
+    public override async Task Consumer(IConsumerContext<SendMessage> context)
+    {
+        var @event = new SendMessageChat(context.Message.IdChat, 
+            context.Message.IdChat, context.Message.Text);
+
+        await _chatProcess.Include(@event).ConfigureAwait(false);
+        context.NotifyConsumed();
+    }
+}
