dda843bbbdc73beb5ef41d3f53a2f672400424c7
Andre Meira <andre._.meira@hotmail.com>
2024-02-12 08:58:42 -0400

    Ajuste no objeto de message.

--- src/Worker/Sample.ChatHub.Core/Chat/Message.cs
+++ src/Worker/Sample.ChatHub.Core/Chat/Message.cs
@@ -3,11 +3,11 @@
 public record Message
 {
     public Guid Id { get; init; }
-    public byte[] Text { get; init; }
+    public string Text { get; init; }
     public Guid UserId { get; init; }
     public DateTime Date { get; init; }
 
-    public Message(byte[] message, Guid userId)
+    public Message(string message, Guid userId)
     {
         Id = Guid.NewGuid();
 
