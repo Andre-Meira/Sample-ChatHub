879d1bd96fcabbdc4f92ffbd779a3bea8143117a
Andre Meira <andre._.meira@hotmail.com>
2024-03-13 14:38:14 -0400

    Adição da interface de comunicao do chat.

---
+++ src/core/Sample.ChatHub.Domain.Contracts/IChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Sample.ChatHub.Domain.Contracts.Messages;
+
+namespace Sample.ChatHub.Domain.Contracts;
+
+public interface IChatHub
+{
+    public Task SendMessage(Guid idChat, string message);
+    public Task ReceiveMessage(ContextMessage context);
+    public Task AckMessage(Guid IdChat, Guid IdMessage);
+}
